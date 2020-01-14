b0747ba964d2d48eedeefb7361be51f8aecd2024
sadok386 <sadok.benfredj@hotmail.com>
2020-01-14 15:01:07 +0100

    RemoveUser

---
+++ SempicAuth/src/main/java/fr/uga/miashs/sempic/backingbeans/DeleteUser.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uga.miashs.sempic.backingbeans;
+
+import fr.uga.miashs.sempic.SempicModelException;
+import fr.uga.miashs.sempic.SempicModelUniqueException;
+import fr.uga.miashs.sempic.dao.SempicAlbumFacade;
+import fr.uga.miashs.sempic.dao.SempicUserFacade;
+import fr.uga.miashs.sempic.entities.SempicUser;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Jerome David <jerome.david@univ-grenoble-alpes.fr>
+ */
+@Named
+@ViewScoped
+public class DeleteUser implements Serializable {
+    
+    private SempicUser current;
+    
+    @Inject
+    private SempicUserFacade userDao;
+    
+    
+
+    
+    public DeleteUser() {
+    }
+    
+    public String delete(SempicUser user ) {
+        //System.out.println(current);
+        
+        try {
+            userDao.delete(user);
+        } 
+        catch (SempicModelUniqueException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("un utilisateur avec cette adresse mail existe déjà"));
+            return "failure";
+        }
+        catch (SempicModelException ex) {
+           FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(ex.getMessage()));
+            return "failure";
+        }
+        
+        return "list-users.xhtml";
+    }
+}
