52eabfb90e042104cfdd397b885ec11e911c9a12
Nicolas <nicolas.gauss.1@orange.fr>
2020-01-17 04:36:18 +0100

    fix

--- SempicAuth/src/main/java/fr/uga/miashs/sempic/backingbeans/Research.java
+++ SempicAuth/src/main/java/fr/uga/miashs/sempic/backingbeans/Research.java
@@ -1,4 +1,4 @@
-PREFIX dbo: <http://dbpedia.org/ontology/>/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
